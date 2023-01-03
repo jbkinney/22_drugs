5a49a6d226a6bad932479e63c8161c637cf040cb
Justin B. Kinney <jkinney@cshl.edu>
2023-01-02 19:53:03 -0500

    added correct smn2 cipher

--- cluster/analysis_smn2_cipher/params.py
+++ cluster/analysis_smn2_cipher/params.py
@@ -10,13 +10,13 @@ import time
 
 # User options
 illumina_run_dir    = '/sonas-hs/kinney/hpc/home/jkinney/big_data/illumina_runs'
-metadata_file       = 'metadata_smn2_cipher.xlsx'
+metadata_file       = './metadata_smn2_cipher.xlsx'
 tmp_dir             = 'tmp'
 interm_dir          = 'intermediate'
 reads_per_split     = int(1E6)
 clean_intermediate  = False
-make_split_files    = False
-make_features_files = False
+make_split_files    = True
+make_features_files = True
 make_counts_files   = True
 merge_feature       = False
 clean_tmp = True
